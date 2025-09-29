2360b8d7a8430553920687ba9f5d71aa09bb6ddb
jhan0121 <jhan0121@gmail.com>
2025-09-29 13:04:04 +0900

    feat: JdbcTemplate 기능 추가
    - 단일 select
    - 다중 select
    - insert/update

--- jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
@@ -1,9 +1,14 @@
 package com.interface21.jdbc.core;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import javax.sql.DataSource;
 
 public class JdbcTemplate {
 
@@ -13,5 +18,85 @@ public class JdbcTemplate {
 
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    public void executeUpdate(
+            final String sql,
+            final Object... parameters
+    ) {
+        try (
+                final Connection connection = dataSource.getConnection();
+                final PreparedStatement preparedStatement = connection.prepareStatement(sql)
+        ) {
+
+            log.debug("query : {}", sql);
+
+            bindParameters(preparedStatement, parameters);
+            preparedStatement.executeUpdate();
+        } catch (final SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public <T> List<T> execute(
+            final String sql,
+            final RowMapper<T> rowMapper,
+            final Object... parameters
+    ) {
+        try (
+                final Connection connection = dataSource.getConnection();
+                final PreparedStatement preparedStatement = connection.prepareStatement(sql)
+        ) {
+            log.debug("query : {}", sql);
+
+            bindParameters(preparedStatement, parameters);
+
+            try (final ResultSet rs = preparedStatement.executeQuery()) {
+                final List<T> result = new ArrayList<>();
+                if (rs.next()) {
+                    result.add(rowMapper.mapRow(rs));
+                }
+                return result;
+            }
+
+        } catch (final SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public <T> T executeObject(
+            final String sql,
+            final RowMapper<T> rowMapper,
+            final Object... parameters
+    ) {
+        try (
+                final Connection connection = dataSource.getConnection();
+                final PreparedStatement preparedStatement = connection.prepareStatement(sql)
+        ) {
+            log.debug("query : {}", sql);
+
+            bindParameters(preparedStatement, parameters);
+            try (final ResultSet resultSet = preparedStatement.executeQuery()) {
+                if (resultSet.next()) {
+                    return rowMapper.mapRow(resultSet);
+                }
+            }
+
+            return null;
+        } catch (final SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void bindParameters(
+            final PreparedStatement preparedStatement,
+            final Object... parameters
+    ) throws SQLException {
+        for (int i = 0; i < parameters.length; i++) {
+            preparedStatement.setString(i + 1, parameters[i].toString());
+        }
     }
 }
