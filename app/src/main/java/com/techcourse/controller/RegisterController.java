e7f8c8841693c74e9401a5ee1baaec44571496b7
jhan0121 <jhan0121@gmail.com>
2025-09-29 13:06:34 +0900

    refactor: Controller UserService 적용

--- app/src/main/java/com/techcourse/controller/RegisterController.java
+++ app/src/main/java/com/techcourse/controller/RegisterController.java
@@ -1,25 +1,38 @@
 package com.techcourse.controller;
 
-import com.techcourse.domain.User;
-import com.techcourse.repository.InMemoryUserRepository;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import com.interface21.webmvc.servlet.view.JspView;
-import com.interface21.webmvc.servlet.ModelAndView;
 import com.interface21.context.stereotype.Controller;
 import com.interface21.web.bind.annotation.RequestMapping;
 import com.interface21.web.bind.annotation.RequestMethod;
+import com.interface21.webmvc.servlet.ModelAndView;
+import com.interface21.webmvc.servlet.view.JspView;
+import com.techcourse.config.DataSourceConfig;
+import com.techcourse.dao.UserDao;
+import com.techcourse.dao.UserHistoryDao;
+import com.techcourse.domain.User;
+import com.techcourse.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @Controller
 public class RegisterController {
 
+    private final UserService userService;
+
+    public RegisterController() {
+        this.userService = new UserService(
+                new UserDao(DataSourceConfig.getInstance()),
+                new UserHistoryDao(DataSourceConfig.getInstance())
+        );
+    }
+
     @RequestMapping(value = "/register", method = RequestMethod.POST)
     public ModelAndView register(final HttpServletRequest request, final HttpServletResponse response) {
-        final var user = new User(2,
+        final var user = new User(
                 request.getParameter("account"),
                 request.getParameter("password"),
-                request.getParameter("email"));
-        InMemoryUserRepository.save(user);
+                request.getParameter("email")
+        );
+        userService.insert(user);
 
         return new ModelAndView(new JspView("redirect:/index.jsp"));
     }
